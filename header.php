8d1f1ec3939406de52fb91c90a28aa39be02c67f
Patrick Ingle <phkcorp2005@gmail.com>
2015-07-21 20:54:36 -0700

    implemented common header with custom title attribute

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
-<?php
-
-?>
 <html>
 <head>
-
-<title>Supply Chain Management</title>
+<?php
+if (isset($title)) {
+	echo '<title>'.$title.'</title>';
+} else {
+	echo '<title>Supply Chain Management</title>';
+}
+?>
 <script src="images/js/scm.js"></script>
 </head>
